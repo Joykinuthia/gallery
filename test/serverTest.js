788b16f31bcb07ae33e3a295fb2a845bb6f5139a
Joykinuthia <joyrosekinuthia@gmail.com>
2025-09-23 11:24:20 +0300

    Fixing errors

--- test/serverTest.js
+++ test/serverTest.js
@@ -13,14 +13,15 @@ describe('Photos', function(){
 
 
     it('should list ALL photos on / GET', function(done){
-        this.timeout(60000);
+        this.timeout(10000);
         chai.request(server)
         .get('/')
-        .end(function(err,res){
+        .end(function(err, res){
+            if (err) return done(err);
             res.should.have.status(200);
             res.should.be.html;
-            res.body.should.be.a('object')
+            res.text.should.include('Photos'); 
             done();
-        })
+        });
     });
 })
